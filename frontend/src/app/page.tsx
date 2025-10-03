4e90a3693faf4823d343faa6cd4f949049c92a77
Rychard <rycgamerpro@gmail.com>
2025-10-02 21:01:04 -0300

    install next14 and antd

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function Home() {
+    return (
+        <div className="flex flex-col h-svh w-full justify-center items-center">
+            <h1>Olá, mundo</h1>
+            <Button type="primary">Button</Button>
+        </div>
+    );
+}
