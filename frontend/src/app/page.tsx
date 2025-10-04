db70338a68e55c64780f7f9646ca476eb1c8adbf
Rychard <rycgamerpro@gmail.com>
2025-10-03 23:08:43 -0300

    refactor: simplify Home component to redirect to /app

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Button } from "antd";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-    return (
-        <div className="flex flex-col h-svh w-full justify-center items-center">
-            <h1>Olá, mundo</h1>
-            <Button type="primary">Button</Button>
-        </div>
-    );
+    return redirect("/app");
 }
