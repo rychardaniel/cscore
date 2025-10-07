ca8d3f1c249d171d13e8c83ae2a2cb844127c786
Rychard <rycgamerpro@gmail.com>
2025-10-06 23:44:37 -0300

    initial header

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,5 +1,5 @@
 export default function App() {
     return (
-        <div className="flex justify-center items-center h-full bg-amber-800">body</div>
+        <div className="flex justify-center items-center h-full">body</div>
     );
 }
--- frontend/src/app/components/app/header.tsx
+++ frontend/src/app/components/app/header.tsx
@@ -1,5 +1,22 @@
+import { Flex } from "antd";
+import { Icon } from "@iconify/react";
+
 type Props = {};
 
 export default function Header({}: Props) {
-    return <div className="w-full bg-amber-600 p-4">header</div>;
+    return (
+        <Flex
+            justify="center"
+            align="center"
+            style={{ height: 65, borderBottom: "2px solid #ddd" }}
+        >
+            <div className="w-full max-w-[1200px] p-4 h-full flex justify-between items-center">
+                <Flex gap={5}>
+                    <Icon icon="iconoir:graduation-cap" className="text-2xl text-(--blue)" />
+                    <h2 className="font-bold">Cscore</h2>
+                </Flex>
+                <div>Cscore</div>
+            </div>
+        </Flex>
+    );
 }
